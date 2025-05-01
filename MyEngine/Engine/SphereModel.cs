b04ddb4e9e3a86b6abdf3deecfdabe319e7f3acd
Serhii Turlak <moltenless@gmail.com>
2025-05-01 13:07:40 +0300

    Add project files.

---
+++ MyEngine/Engine/SphereModel.cs
@@ -0,0 +1,17 @@
+﻿namespace MyEngine.Engine
+{
+    internal class SphereModel
+    {
+        public Point3d Center { get; set; }
+        public double Radius { get; set; }
+        public Rgb GeneralColor { get; set; }
+
+
+        public SphereModel(Point3d center, double radius, Rgb generalColor)
+        {
+            Center = center;
+            Radius = radius;
+            GeneralColor = generalColor;
+        }
+    }
+}
---
+++ MyEngine/MainForm.cs
@@ -0,0 +1,28 @@
+﻿namespace MyEngine
+{
+    public partial class MainForm : Form
+    {
+        private Game.Game game;
+        
+        public MainForm()
+        {
+            InitializeComponent();
+            game = new Game.Game(outputBox, this);
+            DeleteLabel();
+            game.RunGame();
+        }
+
+        private void DeleteLabel()
+        {
+            var timer = new System.Windows.Forms.Timer();
+            timer.Interval = 5000;
+            timer.Tick += (s, e) =>
+            {
+                Controls.Remove(label);
+                label.Dispose();
+                timer.Stop();
+            };
+            timer.Start();
+        }
+    }
+}
